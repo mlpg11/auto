3e003ba97d56c287c9452ba92c126938155faf42
gustavo-villar-dev <gustavovm@dcc.ufrj.br>
2023-11-28 20:51:05 -0300

    routing

--- auto-front/src/App.js
+++ auto-front/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Explorar from './components/Explorar';
-
-
+import MeusTokens from './components/MeusTokens';
+import NotFound from './components/NotFound';
 function App() {
   return (
     <Router>
       <div className="App">
-        <Explorar></Explorar>
+        <Routes>
+          <Route exact path="/" element={<Explorar/>}/>
+          <Route exact path="/explorar" element={<Explorar/>}/>
+          <Route exact path="/meus-tokens" element={<MeusTokens/>} />
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
       </div>
     </Router>
   );
