b877e53c15f6f0bf38fc9fcd29eda70fcdaf6b3b
gustavo-villar-dev <gustavovm@dcc.ufrj.br>
2023-11-28 20:52:06 -0300

    meus tokens page

---
+++ auto-front/src/components/MeusTokens.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import FilterBar from './FilterBar';
+import SortBar from './SortBar';
+import Header from './Header';
+import ListaCards from './ListaCards';
+
+function Explorar() {
+    // filter bar
+    const [filters, setFilters] = useState({
+        sortOrder: 'titulo',
+        tesouroDireto: true,
+        outros: false,
+        searchTerm: ''
+    });
+
+    const handleFilterChange = (newFilters) => {
+        setFilters(newFilters);
+        // Aqui você também pode fazer chamadas para atualizar os dados exibidos
+        // com base nos novos filtros
+    };
+
+    // sort bar
+    const [sorting, setSorting] = useState({
+        titulo: '',
+        rentabilidade: '',
+        vencimento: '',
+        minimo: '',
+        risco: '',
+    });
+
+    const handleSortChange = (newSorting) => {
+        setSorting(newSorting);
+        // Atualize os dados exibidos com base na nova ordenação
+    };
+
+    return (
+        <div>
+            <Header></Header>
+            <SortBar onSortChange={handleSortChange} />
+            <FilterBar onFilterChange={handleFilterChange} />
+        </div>
+    );
+}
+
+export default Explorar;
