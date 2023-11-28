a136b5c710154c07312ea4453ca883a966b9b6a4
gustavo-villar-dev <gustavovm@dcc.ufrj.br>
2023-11-28 20:51:55 -0300

    dynamic grid

--- auto-front/src/components/ListaCards.js
+++ auto-front/src/components/ListaCards.js
@@ -5,8 +5,72 @@ import './ListaCards.css';
 function ListaCards() {
     const cards = [
         {
+            id: '0',
             titulo: 'Tesouro Selic',
             tipo: 'selic',
+            rentabilidade: '3,42% a.a',
+            vencimento: '15/07/2025',
+            investimento_minimo: 'R$ 152,00',
+            risco: 'Médio'
+        },
+        {
+            id: '1',
+            titulo: 'Tesouro Selic',
+            tipo: 'prefixado',
+            rentabilidade: '3,42% a.a',
+            vencimento: '15/07/2025',
+            investimento_minimo: 'R$ 152,00',
+            risco: 'Médio'
+        },
+        {
+            id: '2',
+            titulo: 'Tesouro Selic',
+            tipo: 'ipca+',
+            rentabilidade: '3,42% a.a',
+            vencimento: '15/07/2025',
+            investimento_minimo: 'R$ 152,00',
+            risco: 'Médio'
+        },
+        {
+            id: '3',
+            titulo: 'Tesouro Selic',
+            tipo: 'renda+',
+            rentabilidade: '3,42% a.a',
+            vencimento: '15/07/2025',
+            investimento_minimo: 'R$ 152,00',
+            risco: 'Médio'
+        },
+        {
+            id: '4',
+            titulo: 'Tesouro Selic',
+            tipo: 'outros',
+            rentabilidade: '3,42% a.a',
+            vencimento: '15/07/2025',
+            investimento_minimo: 'R$ 152,00',
+            risco: 'Médio'
+        },
+        {
+            id: '5',
+            titulo: 'Tesouro Selic',
+            tipo: 'renda+',
+            rentabilidade: '3,42% a.a',
+            vencimento: '15/07/2025',
+            investimento_minimo: 'R$ 152,00',
+            risco: 'Médio'
+        },
+        {
+            id: '6',
+            titulo: 'Tesouro Selic',
+            tipo: 'ipca+',
+            rentabilidade: '3,42% a.a',
+            vencimento: '15/07/2025',
+            investimento_minimo: 'R$ 152,00',
+            risco: 'Médio'
+        },
+        {
+            id: '7',
+            titulo: 'Tesouro Selic',
+            tipo: 'renda+',
             rentabilidade: '3,42% a.a',
             vencimento: '15/07/2025',
             investimento_minimo: 'R$ 152,00',
@@ -15,18 +79,20 @@ function ListaCards() {
     ];
 
     return (
-        <div id="main">
-        {cards.map((card) => (
-            <Card
-            titulo={card.titulo}
-            tipo={card.tipo}
-            rentabilidade={card.rentabilidade}
-            vencimento={card.vencimento}
-            investimento_minimo={card.investimento_minimo}
-            risco={card.risco}
-            />
-        ))}
+        <div className='grid-cards'>
+            {cards.map((card) => (
+                <Card
+                key={card.id}
+                titulo={card.titulo}
+                tipo={card.tipo}
+                rentabilidade={card.rentabilidade}
+                vencimento={card.vencimento}
+                investimento_minimo={card.investimento_minimo}
+                risco={card.risco}
+                />
+            ))}
         </div>
+        
     );
 }
 
